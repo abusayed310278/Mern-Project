0ad7a2d54ef29c8d2e4d569096315eacaead361c
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-04-25 08:40:11 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+export default function App() {
+  return (
+    <>
+      <h1>My React App</h1>
+
+    </>
+  )
+}
